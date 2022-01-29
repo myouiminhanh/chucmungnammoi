1f3b2c26d5b65348bb92a35cf3582d58da7c6f76
myouiminhanh <98657329+myouiminhanh@users.noreply.github.com>
2022-01-30 00:07:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Puzzlies à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Lạc vào ngôi nhà xếp hình
+    Ta gọi đây là định mệnh`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Năm cũ đã qua, năm mới sẽ đến',
+    desc: 'Em đã sẵn sàng để bắt đầu năm 2022 cùng anh chị chưa?',
+    btnYes: 'Ever ready',
+    btnNo: 'Never ready',
+    question:'Em đoán xem tại sao CNN có 17 CLB mà em lại thuộc về CNN Puzzles',
+    btnReply: 'Gửi cho anh chị <3',
+    reply: 'Vì đây là số phận vũ trụ sắp đặt cho chúng ta <33333333',
+    mess: 'Anh chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chúc em năm mới vui vẻ nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/watch/?ref=search&v=970313027169950&external_log_id=f8f2d295-910a-4574-bd21-97ef9269e639&q=l%E1%BA%A1%20l%C3%B9ng%20n%C4%83m%20m%E1%BB%9Bi' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
